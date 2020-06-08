724bad77b60ae7f4470c70cb2cf247a2de437710
chriskay25 <chriskay25@gmail.com>
2020-06-08 12:15:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let element = document.getElementById("main");
+element.remove();
+
+// Create an <h1> with id "victory"
+let newHeader = document.createElement('h1');
+newHeader.className = "victory";
+newHeader.id = "victory"
+newHeader.innerHTML = "Chris is the champion";
+document.body.appendChild(newHeader); 
